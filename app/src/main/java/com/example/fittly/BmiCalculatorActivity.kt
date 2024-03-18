f4c1b9d821458a4423f0ad6c3d071c03d0a5e46e
Bartłomiej Kubicki <kubicb331_aehit@students.vizja.pl>
2024-03-18 20:07:42 +0100

    Upgrades

--- app/src/main/java/com/example/fittly/BmiCalculatorActivity.kt
+++ app/src/main/java/com/example/fittly/BmiCalculatorActivity.kt
@@ -1,10 +1,10 @@
 package com.example.fittly
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-
 class BmiCalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +19,14 @@ class BmiCalculatorActivity : AppCompatActivity() {
             val weight = editTextWeight.text.toString()
 
             val announcement = "Wzrost: $height\nWaga: $weight"
+        }
+
+        // Find "HOME" button and add an event listener to it
+        val homeButton: Button = findViewById(R.id.homeButton)
+        homeButton.setOnClickListener {
+            // "BMI" button click action
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
