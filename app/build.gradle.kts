06d3a10244590debc787d1b3291cdb98194b163c
Bartłomiej Kubicki <kubicb331_aehit@students.vizja.pl>
2024-03-18 14:01:55 +0100

    New bmi activity, new bmi xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,10 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -59,6 +63,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/fittly/MainActivity.kt
+++ app/src/main/java/com/example/fittly/MainActivity.kt
@@ -3,22 +3,25 @@ package com.example.fittly
 import android.os.Bundle
 import android.widget.Button
 import android.app.Activity
+import android.content.Intent
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Znajdź przycisk "LOG IN" i dodaj do niego nasłuchiwacz zdarzeń
+        // Find "LOG IN" button and add an event listener to it
         val loginButton: Button = findViewById(R.id.loginButton)
         loginButton.setOnClickListener {
-            // Obsługa kliknięcia przycisku "LOG IN"
+            // "LOG IN" button click action
         }
 
-        // Znajdź przycisk "BMI" i dodaj do niego nasłuchiwacz zdarzeń
+        // Find "BMI" button and add an event listener to it
         val bmiButton: Button = findViewById(R.id.bmiButton)
         bmiButton.setOnClickListener {
-            // Obsługa kliknięcia przycisku "BMI"
+            // "BMI" button click action
+            val intent = Intent(this, BmiCalculatorActivity::class.java)
+            startActivity(intent)
         }
     }
 }
